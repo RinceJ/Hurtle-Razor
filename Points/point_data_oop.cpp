691bc9d6a8a07ebdea43f99baca7754a1ff7b05e
RinceJ <rincethomasthekkekara@gmail.com>
2019-02-21 12:25:15 +0530

    Changes to code

--- Points/point_data_oop.cpp
+++ Points/point_data_oop.cpp
@@ -1,32 +1,76 @@
 
 //Author:Hurtle_Rzr$
-// 
+// Academic code regarding the distance between two cartesian co-oridnates.
 
 #include <iostream>
 #include <cstring>
-
+#include <math.h>
 
 
 // to do // bug on same ID number on multiple points.
 // to do // data structure on storing key co -ordinates
 
-using namespace std;
 
-class A
+
+class point
 {
 	private:	
 		char *m_LABEL;
-		const unsigned int m_ID;
+		unsigned int m_ID;
+		double *ptr;
 
 		
 	public:	
-		A(const char *LABEL, const unsigned int ID):m_ID(ID)
+		point(const char *LABEL, const unsigned int ID, const double *PT):m_ID(ID)
 		{
 			m_LABEL = NULL;
 			m_LABEL = new char(strlen(LABEL));
 			strcpy(m_LABEL, LABEL);
-			
+			ptr = NULL;
+			ptr = new double[3];
+			for (int i = 0; i < 3; i++)
+			{
+				ptr[i] = PT[i];
+			}		
 		}		
+
+		void operator=(const point &obj)
+			{
+				strcpy(m_LABEL, obj.m_LABEL);
+				m_ID = obj.m_ID;
+				for (int i = 0; i < 3; i++)
+				{
+					ptr[i] = obj.ptr[i];
+				}
+			}	
+
+	// to do creating multiple instances. //demo overloading
+		// A &operator+(const A &obj)
+		// {	
+		// 	double *temp = new double[3];
+		// 	A *ptr_add = new A(temp);
+		// 	for (int i = 0; i < 3; i++)
+		// 	{
+		// 		ptr_add->ptr[i] = ptr[i] + obj.ptr[i];  /// 
+		// 	}
+		// 	return *ptr_add;
+		// 	delete[] temp;
+		// }
+
+
+		point &operator-(const point &obj)
+		{
+			double *temp = new double[3];
+			point *ptr_add = new point("", 0, temp);
+			for (int i = 0; i < 3; i++)
+			{
+				ptr_add->ptr[i] = pow(ptr[i] - obj.ptr[i], 2);  /// 
+			}
+			return *ptr_add;
+			delete[] temp;
+		}
+
+
 		void display_forward()
 		{
 		  	for ( int i =0; i< strlen(m_LABEL); i++)
@@ -35,7 +79,8 @@ class A
 			}
 			std::cout << "ID number of the point is " << m_ID << std::endl;
 			std::cout << '\n' ;
-		}		
+		}	
+
 		void display_reverse()
 		{
 		  	for ( int i = strlen(m_LABEL)-1 ; i>-1; i--)
@@ -44,24 +89,50 @@ class A
 			}
 			//std::cout << "" << m_ID << std::endl;
 			std::cout << '\n' ;
-		}		
-		~A()
+		}	
+
+		void display_distance()
 		{
-			std::cout << "Destructor has been called " << std::endl;
+			double temp = 0.0;
+			for (int i = 0; i < 3; i++)
+			{
+				//std::cout << *(ptr + i) << std::endl;
+
+				temp += *(ptr + i);
+				
+			}
+			std::cout << sqrt(temp) << std::endl;
+		}	
+		~point()
+		{
+			delete[] ptr;
 		}		
+
 };
 
 
 int main()
 {
-		A *ptr = new A("point_1", 1);
-		
-		ptr->display_forward();		
-		//ptr->display_reverse();		
-		delete ptr;		
-		A *ptr1 = new A("point_2", 1);
-		
-		ptr1->display_forward();
-		
-		delete ptr1;
+
+		double a[] = { 10, 10, 10 };
+		double b[] = { 5, 5, 5 };
+		double c[3];
+
+		point *ptr_1 = new point("point_1", 1, a);	
+		ptr_1->display_forward();		
+		point *ptr_2 = new point("point_2", 2, b);	
+		std::cout << '\n';
+		ptr_2->display_forward();
+	
+
+		point *ptr_3 = new point("", 0, c); 
+		*ptr_3 = *ptr_1 - *ptr_2;   //to do alternate approach to attack the distance problem.
+		point *ptr_4;
+		ptr_4 = ptr_3;
+		ptr_4->display_distance();
+
+
+		ptr_3->display_distance();
+
+		delete ptr_3;
 }
